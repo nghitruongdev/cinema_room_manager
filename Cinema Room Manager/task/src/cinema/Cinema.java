e6346bdfe68a84543f5f6f5a8309297ea3c17557
Nghi Truong <89492293+nghitv2410@users.noreply.github.com>
2021-08-26 17:47:00 +0700

    Stage 2 completed, caculate the revenue for the theatre depending on the number of seats

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,20 +1,34 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
-        for (int i = 0; i < 8; i++) {
-            if (i == 0) {
-                System.out.print("Cinema:\n  ");
-                for (int j = 1; j < 9; j++) {
-                    System.out.print(j + " ");
-                }
-                System.out.println();
-            } else {
-                System.out.println(i + " " + "S ".repeat(8));
-            }
-
+        Scanner scanner = new Scanner(System.in);
+        //read inputs and find total seats in a room
+        //rows and seats no greater than 9
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seatPerRow = scanner.nextInt();
+        if (rows > 9 || seatPerRow > 9) {
+            return;
         }
+        int totalSeats = rows * seatPerRow;
+
+        //total <=60, price is $10
+        //total >60, first half rows will be $10, other half $8 (first half<=other half)
+        //find sum revenue
+        int revenue;
+        if (totalSeats <= 60) {
+            revenue = 10 * totalSeats;
+        } else {
+            int halfFirst = rows / 2;
+            revenue = seatPerRow * (halfFirst * 10 + (rows - halfFirst) * 8);
+        }
+        System.out.println("Total income:\n$" + revenue);
+        scanner.close();
     }
 }
