425a996458792d256b1ae244117a1b246f273e1f
Nghi Truong <89492293+nghitv2410@users.noreply.github.com>
2021-08-26 17:20:12 +0700

    Stage 1 completed

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,5 +4,17 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        for (int i = 0; i < 8; i++) {
+            if (i == 0) {
+                System.out.print("Cinema:\n  ");
+                for (int j = 1; j < 9; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            } else {
+                System.out.println(i + " " + "S ".repeat(8));
+            }
+
+        }
     }
 }
